889aa95fd915b693f87ae97ad9ba0608c44cab61
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-05-28 14:05:15 -0500

    feat(restaurant): implement user validation adapter

--- src/main/java/com/pragma/plazoleta/infrastructure/out/feign/adapter/UserValidationAdapter.java
+++ src/main/java/com/pragma/plazoleta/infrastructure/out/feign/adapter/UserValidationAdapter.java
@@ -4,6 +4,7 @@ import com.pragma.plazoleta.domain.model.UserInformation;
 import com.pragma.plazoleta.domain.spi.IUserValidationPort;
 import com.pragma.plazoleta.infrastructure.out.feign.client.IUserFeignClient;
 import com.pragma.plazoleta.infrastructure.out.feign.mapper.IUserFeignMapper;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -14,6 +15,13 @@ public class UserValidationAdapter implements IUserValidationPort {
 
     @Override
     public UserInformation getUserById(Long id) {
-        return null;
+        try {
+            var dto = userFeignClient.getUserById(id);
+
+            return userFeignMapper.toUserInformation(dto);
+        } catch (FeignException.NotFound e) {
+
+            return null;
+        }
     }
 }
