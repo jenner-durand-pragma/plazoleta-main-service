c1cb2f69205bbb122cc6a7f354eb03d8b15c7b22
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-05-28 13:45:11 -0500

    feat(restaurant): implement restaurant use case
    Implement createRestaurant method

--- src/main/java/com/pragma/plazoleta/domain/usecase/RestaurantUseCase.java
+++ src/main/java/com/pragma/plazoleta/domain/usecase/RestaurantUseCase.java
@@ -1,6 +1,9 @@
 package com.pragma.plazoleta.domain.usecase;
 
 import com.pragma.plazoleta.domain.api.IRestaurantServicePort;
+import com.pragma.plazoleta.domain.enums.Roles;
+import com.pragma.plazoleta.domain.exception.restaurant.NitAlreadyExistsException;
+import com.pragma.plazoleta.domain.exception.restaurant.UserIsNotOwnerException;
 import com.pragma.plazoleta.domain.model.Restaurant;
 import com.pragma.plazoleta.domain.spi.IRestaurantPersistencePort;
 import com.pragma.plazoleta.domain.spi.IUserValidationPort;
@@ -14,6 +17,25 @@ public class RestaurantUseCase implements IRestaurantServicePort {
 
     @Override
     public Restaurant createRestaurant(Restaurant restaurant) {
-        return null;
+        validateOwner(restaurant.getOwnerId());
+        validateNitUniqueness(restaurant.getNit());
+
+        return restaurantPersistencePort.save(restaurant);
+    }
+
+    private void validateOwner(Long ownerId) {
+        var user = userValidationPort.getUserById(ownerId);
+
+        if (user == null || !Roles.OWNER.getName().equals(user.getRoleName())) {
+            throw new UserIsNotOwnerException();
+        }
+    }
+
+    private void validateNitUniqueness(String nit) {
+        var existsByNit = restaurantPersistencePort.existsByNit(nit);
+
+        if (Boolean.TRUE.equals(existsByNit)) {
+            throw new NitAlreadyExistsException();
+        }
     }
 }
