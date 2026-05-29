2a0a87af3079cf8ee216ab491bfcfd852b063ef1
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-05-29 11:24:08 -0500

    fix(dish): adjust exception message in DishOwnershipException

--- src/main/java/com/pragma/plazoleta/domain/exception/dish/DishOwnershipException.java
+++ src/main/java/com/pragma/plazoleta/domain/exception/dish/DishOwnershipException.java
@@ -4,7 +4,7 @@ import com.pragma.plazoleta.domain.exception.BusinessRuleException;
 
 public class DishOwnershipException extends BusinessRuleException {
 
-    private static final String ERROR_MESSAGE = "Only the restaurant owner can create dishes in this restaurant.";
+    private static final String ERROR_MESSAGE = "Only the restaurant owner can manage dishes in this restaurant.";
 
     public DishOwnershipException() {
         super(ERROR_MESSAGE);
