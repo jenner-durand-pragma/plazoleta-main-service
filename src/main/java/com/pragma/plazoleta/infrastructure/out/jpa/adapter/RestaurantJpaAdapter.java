152e6c1ae68a4591c4d1a05d0175394c2da7a7a2
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-05-29 00:35:02 -0500

    refactor(restaurant): remove unused import

--- src/main/java/com/pragma/plazoleta/infrastructure/out/jpa/adapter/RestaurantJpaAdapter.java
+++ src/main/java/com/pragma/plazoleta/infrastructure/out/jpa/adapter/RestaurantJpaAdapter.java
@@ -2,7 +2,6 @@ package com.pragma.plazoleta.infrastructure.out.jpa.adapter;
 
 import com.pragma.plazoleta.domain.model.Restaurant;
 import com.pragma.plazoleta.domain.spi.IRestaurantPersistencePort;
-import com.pragma.plazoleta.infrastructure.out.jpa.entity.RestaurantEntity;
 import com.pragma.plazoleta.infrastructure.out.jpa.mapper.IRestaurantEntityMapper;
 import com.pragma.plazoleta.infrastructure.out.jpa.repository.IRestaurantRepository;
 import lombok.RequiredArgsConstructor;
