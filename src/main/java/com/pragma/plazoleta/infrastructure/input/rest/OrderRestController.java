6ea1b8e0e1dcfefe11ef7e1602b43cf53b43a9a3
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-06-01 10:00:19 -0500

    test(order): add test to assign order to in preparation in rest controller

--- src/main/java/com/pragma/plazoleta/infrastructure/input/rest/OrderRestController.java
+++ src/main/java/com/pragma/plazoleta/infrastructure/input/rest/OrderRestController.java
@@ -18,6 +18,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,5 +71,14 @@ public class OrderRestController {
                         size
                 )
         );
+    }
+
+    @IsEmployee
+    @PatchMapping("/{orderId}/assign")
+    public ResponseEntity<OrderResponseDto> assignOrder(
+            @PathVariable Long orderId,
+            @AuthenticationPrincipal AuthenticatedUser authenticatedUser
+    ) {
+        return null;
     }
 }
